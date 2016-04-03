d2bde3d7cfbd9dbc31077991cfad9eb94d14632b
syoels <syoels@gmail.com>
2016-04-03 19:52:39 +0300

    all

--- Assets/Scripts/thesims/TeamFirewood/UnknownPile.cs
+++ Assets/Scripts/thesims/TeamFirewood/UnknownPile.cs
@@ -13,14 +13,15 @@ namespace TeamFirewood {
 			state ["searchedHere"] = new StateValue(false);
 			foreach(Item item in possibleItemsInPile){
 				bool val = RandomUtils.RandBool (0.2f);
-				state["has" + item] = new StateValue(val);
+				int number = val ? 1 : 0;
+				state[item.ToString()] = new StateValue(number);
 				Debug.LogError (val);
 			}
 			base.Awake();
 		}
 
 		public void removeItem (Item item){
-			state["has" + item] = new StateValue(false);
+			state[item.ToString()] = new StateValue(0);
 		}
 
 		public void setSearched(bool searchedHere){
